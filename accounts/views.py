46e3f4c2378aa3c93c49beb688be66b7b83377f0
Simona Vycaite <simona.vycaite@gmail.com>
2022-12-09 17:37:37 +0200

    Changed profile menu, added logout

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from accounts.models import Account
 from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
@@ -44,5 +45,8 @@ def login(request):
         
     return render(request, 'login.html')
 
+@login_required(login_url='login')
 def logout(request):
-    return
+    auth.logout(request)
+    messages.success(request, 'Logged Out')
+    return redirect('login')
