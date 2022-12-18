226ea5b92fdbe2a501db4eb224ea25a830ce2c96
Simona Vycaite <simona.vycaite@gmail.com>
2022-12-18 11:46:30 +0200

    fixed reviews

--- offers/urls.py
+++ offers/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.offers, name='offers'),
     path('<slug:category_slug>/', views.offers, name='offers_by_category'),
+    path('post_review/<int:purchase_id>/', views.post_review, name='post_review'),
     path('<slug:category_slug>/<slug:slug>/', views.purchase_detail, name='purchase_detail'),
     path('post_review/<int:purchase_id>/', views.post_review, name='post_review'),
 ]
