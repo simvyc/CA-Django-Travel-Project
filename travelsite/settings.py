e2ae2338a13de1b657505d7bb4d26408ce1703a2
Simona <67691428+simvyc@users.noreply.github.com>
2026-01-09 14:33:22 +0200

    Update settings.py

--- travelsite/settings.py
+++ travelsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e56wzg(@mc8kdhu(--&9^3dcx0f(gn#27xgd%@kgpt$c36w#$o'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
